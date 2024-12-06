387da8dfc3d5a5c0b60390c8a05588c3266ee53c
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-12-06 15:58:36 +0700

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
