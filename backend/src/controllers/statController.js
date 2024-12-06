31f26156b777319065da3a71de7c9d1c2e18c328
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-12-06 21:42:28 +0700

    create stat api

---
+++ backend/src/controllers/statController.js
@@ -0,0 +1,43 @@
+import Song from "../models/songModel.js";
+import Album from "../models/albumModel.js";
+import User from "../models/userModel.js";
+
+const getStats = async (req, res) => {
+  try {
+    const [totalSongs, totalAlbums, totalUsers, uniqueArtists] =
+      await Promise.all([
+        Song.countDocuments(),
+        Album.countDocuments(),
+        User.countDocuments(),
+
+        Song.aggregate([
+          {
+            $unionWith: {
+              coll: "albums",
+              pipeline: [],
+            },
+          },
+          {
+            $group: {
+              _id: "$artist",
+            },
+          },
+          {
+            $count: "count",
+          },
+        ]),
+      ]);
+
+    res.status(200).json({
+      totalSongs,
+      totalAlbums,
+      totalUsers,
+      uniqueArtists: uniqueArtists[0].count || 0,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { getStats };
--- backend/src/routes/statRoutes.js
+++ backend/src/routes/statRoutes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { getStats } from "../controllers/statController.js";
+import {
+  protectRoute,
+  portectAdminRoute,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
+router.get("/", protectRoute, portectAdminRoute, getStats);
 
 export default router;
