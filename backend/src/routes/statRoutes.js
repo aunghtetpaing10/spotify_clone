4800be46ec923cec56cfb9336abf69b183521f2a
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-12-06 22:49:05 +0700

    fix typo

--- backend/src/routes/statRoutes.js
+++ backend/src/routes/statRoutes.js
@@ -2,11 +2,11 @@ import express from "express";
 import { getStats } from "../controllers/statController.js";
 import {
   protectRoute,
-  portectAdminRoute,
+  protectAdminRoute,
 } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, portectAdminRoute, getStats);
+router.get("/", protectRoute, protectAdminRoute, getStats);
 
 export default router;
