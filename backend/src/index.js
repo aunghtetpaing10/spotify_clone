5fc97585bc7c586c586a332e2e6a7d6fd0fab19a
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-12-06 16:13:09 +0700

    set routes and connect to mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import songRoutes from "./routes/songRoutes.js";
+import albumRoutes from "./routes/albumRoutes.js";
+import statRoutes from "./routes/statRoutes.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -14,7 +20,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
