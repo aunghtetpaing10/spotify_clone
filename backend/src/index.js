92d909eb118cd5b0e709bc6f88c284c29f0be677
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-12-11 23:19:44 +0700

    project finished

--- backend/src/index.js
+++ backend/src/index.js
@@ -70,6 +70,14 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 httpServer.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   connectDB();
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axiosModule from "axios";
 
 export const axios = axiosModule.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/api",
 });
